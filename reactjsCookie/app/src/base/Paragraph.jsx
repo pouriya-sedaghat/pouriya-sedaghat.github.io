e19518ac35e13db1a4c1f4924c4ee52281f2aacf
pouriya <pouriyac12@gmail.com>
2024-01-02 20:29:56 +0330

    remove

--- reactjsCookie/app/src/base/Paragraph.jsx
+++
@@ -1,8 +0,0 @@
-const Paragraph = ({ role, value }) => {
-    return (
-        <>
-            <p>{role} : {value}</p>
-        </>
-    );
-}
-export default Paragraph;
--- reactjsCookie/app/src/hooks/useInputHandler.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-export const useInputHandler = (initial) => {
-    const [value, setValue] = useState(initial);
-    const valueHandler = (e) => {
-        setValue(e.target.value);
-    }
-    return [value, setValue, valueHandler];
-}
